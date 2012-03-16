9b22cd1256aa775f5167ea795f680998db9898a0
Erich Bracht <erichbracht@gmail.com>
2012-03-15 22:34:30 -0600

    added welcome activity (basically screen)

--- gen/com/friendlywagerapp/R.java
+++ gen/com/friendlywagerapp/R.java
@@ -14,9 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int createwager=0x7f050007;
         public static final int email=0x7f050000;
         public static final int emailText=0x7f050001;
+        public static final int findwager=0x7f050008;
+        public static final int help=0x7f05000a;
         public static final int login=0x7f050004;
+        public static final int logout=0x7f050009;
         public static final int password=0x7f050002;
         public static final int passwordText=0x7f050003;
         public static final int register=0x7f050006;
@@ -24,14 +28,19 @@ public final class R {
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int welcome=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int email=0x7f040004;
-        public static final int hello=0x7f040000;
-        public static final int login=0x7f040002;
-        public static final int noAccount=0x7f040006;
-        public static final int password=0x7f040005;
-        public static final int register=0x7f040003;
+        public static final int app_name=0x7f040000;
+        public static final int createWager=0x7f040007;
+        public static final int email=0x7f040003;
+        public static final int findWager=0x7f040008;
+        public static final int help=0x7f04000a;
+        public static final int login=0x7f040001;
+        public static final int logout=0x7f040009;
+        public static final int noAccount=0x7f040005;
+        public static final int password=0x7f040004;
+        public static final int register=0x7f040002;
+        public static final int welcome=0x7f040006;
     }
 }
--- src/com/friendlywagerapp/FriendlyWager.java
+++ src/com/friendlywagerapp/FriendlyWager.java
@@ -18,6 +18,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -43,7 +44,9 @@ public class FriendlyWager extends Activity {
 	}
    
    public void onRegisterClicked(View v) {
-	   Toast.makeText(FriendlyWager.this, "Opening Registration Page", Toast.LENGTH_SHORT).show();
+	   // For now just go to welcome screen
+	   Intent goToWelcomePage = new Intent(FriendlyWager.this, Welcome.class);
+	   startActivity(goToWelcomePage);
    }
    
 	private void loginToDatabase(String username, String password) {
