d2ae1c41cb82045894c6bd2556697e13f08e538b
Marty Fahey <martyfahey21@gmail.com>
2012-03-26 00:41:08 -0400

    login function added to talk to server

--- src/com/friendlywagerapp/FriendlyWager.java
+++ src/com/friendlywagerapp/FriendlyWager.java
@@ -7,25 +7,31 @@ import java.util.ArrayList;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class FriendlyWager extends Activity {
 	/** Called when the activity is first created. */
+	
+	private static final String TAG = "FriendlyWager";
 
    @Override
    public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,7 @@ public class FriendlyWager extends Activity {
 	    final EditText passwordField = (EditText) findViewById(R.id.passwordText);
 	    String password = passwordField.getText().toString();
 	    String msg = "email: " + email + "\npassword:" + password;
+	    Log.d(TAG,msg);
 	    Toast.makeText(FriendlyWager.this, msg, Toast.LENGTH_SHORT).show();
 	    loginToDatabase(email, password);
 	}
@@ -49,38 +56,32 @@ public class FriendlyWager extends Activity {
 	   startActivity(goToWelcomePage);
    }
    
+   public static void main(String[] args){
+	   //loginToDatabase("test","test");
+   }
+   
 	private void loginToDatabase(String username, String password) {
 		String result = "";
 		// the year data to send
-		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(4);
+		nameValuePairs.add(new BasicNameValuePair("submit", "Login"));
 		nameValuePairs.add(new BasicNameValuePair("username", username));
 		nameValuePairs.add(new BasicNameValuePair("password", password));
+		nameValuePairs.add(new BasicNameValuePair("remember","0"));
 
 		// http post
 		try {
-			HttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppost = new HttpPost("http://friendlywagerapp.com/login.php");
+			HttpParams params = new BasicHttpParams();
+			params.setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+			HttpClient httpclient = new DefaultHttpClient(params);
+			HttpPost httppost = new HttpPost("http://friendlywagerapp.com/session.php");
 			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-			HttpResponse response = httpclient.execute(httppost);
-			HttpEntity entity = response.getEntity();
-			InputStream is = entity.getContent();
-			// convert response to string
-			try {
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(is, "iso-8859-1"), 8);
-				StringBuilder sb = new StringBuilder();
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					sb.append(line + "\n");
-				}
-				is.close();
+			// Execute HTTP Post Request
+	        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+			String response = httpclient.execute(httppost, responseHandler);
+			Toast.makeText(FriendlyWager.this, response, Toast.LENGTH_SHORT).show();
 
-				result = sb.toString();
-			} catch (Exception e) {
-				System.out.println("Error converting result " + e.toString());
-			}
-
-			// parse json data
+			/* parse json data
 			try {
 				JSONArray jArray = new JSONArray(result);
 				for (int i = 0; i < jArray.length(); i++) {
@@ -92,9 +93,9 @@ public class FriendlyWager extends Activity {
 				}
 			} catch (JSONException e) {
 				System.out.println("Error parsing data " + e.toString());
-			}
+			}*/
 		} catch (Exception e) {
-			System.out.println("Error in http connection " + e.toString());
+			Toast.makeText(FriendlyWager.this, "Error in http connection " + e.toString(), Toast.LENGTH_SHORT).show();
 		}
 	}
 }
